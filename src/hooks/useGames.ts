bf0899e85c2eb238146f6f73fc5f1d4db9a6c47c
muhammmedfahim4321 <fahimmuhammedfahimkp@gmail.com>
2024-02-20 18:58:01 +0530

    displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,9 +1,19 @@
 import { useEffect,useState } from "react";
 import apiClient,{AxiosError, CanceledError} from "../services/api-client";
 
-interface Game {
+
+
+export interface Platform{
+    id:number;
+    name:string;
+    slug:string;
+}
+ 
+export interface Game {
     id: number;
     name: string;
+    background_image:string;
+    parent_platforms: { platform: Platform}[];
 }
   
 interface FetchGamesRespones {
