dcb72a8e4e8b4e4ffabf15187dd57ddd21078e4f
muhammmedfahim4321 <fahimmuhammedfahimkp@gmail.com>
2024-02-20 15:25:28 +0530

    created custom hook to fetch data

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,10 +1,12 @@
-import axios from "axios";
+import axios,{CanceledError,AxiosError} from "axios";
 
 
 export default axios.create({
-    
+
     baseURL:'https://api.rawg.io/api',
     params: {
         key: '3d5f2e475b294a829d22946f6813bc6e'
     }
 })
+
+export { CanceledError , AxiosError }
